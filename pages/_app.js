12cb5c677b9012405108484356a60fb3efa2076f
larssantura <larssantura@gmail.com>
2022-01-17 13:13:25 +0200

    v1.3

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,11 @@ import Head from "next/head";
 
 function MyApp({ Component, pageProps, router }) {
   <Head>
+    <meta property="og:image" content="/Images/thumpnail.PNG" />
+
+    <meta property="og:image:width " content="1914" />
+    <meta property="og:image:type" content="image/png" />
+    <meta property="og:image:height" content="860" />
     <title>Übermensch | Your Gateway to Anther World</title>
   </Head>;
   return (
--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head />
+      <meta property="og:image" content="/Images/thumpnail.PNG" />
+
+      <meta property="og:image:width " content="1914" />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:height" content="860" />
       <title>Übermensch | Your Gateway to Anther World</title>
       <link rel="icon" href="/Images/favicon.svg" type="image/x-icon"></link>
       <body>
